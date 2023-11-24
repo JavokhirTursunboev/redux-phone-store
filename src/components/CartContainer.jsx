1620725dc5016a1c0a98a080c762ef125a83d5f3
Javokhir <jtursunboev@bk.ru>
2023-11-24 21:00:59 +0900

    cartItem and clear btn done

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,41 +1,44 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import CartItem from "./CartItem";
-
+import { clearCart } from "../features/cart/cartSlice";
 function CartContainer() {
+  const dispatch = useDispatch();
+  const { amount, total, cartItems } = useSelector((store) => store.cart);
 
-const {amount, total, cartItems}=useSelector((store) =>   store.cart)
-
-if(amount < 1){
-    return <section className="cart">
+  if (amount < 1) {
+    return (
+      <section className="cart">
         <header>
-            <h2>you bag</h2>
-            <h4 className="empty-cart"> is currently empty</h4>
+          <h2>you bag</h2>
+          <h4 className="empty-cart"> is currently empty</h4>
         </header>
-    </section>
-}
+      </section>
+    );
+  }
 
-  return <section className="cart">
-    <header>
+  return (
+    <section className="cart">
+      <header>
         <h2>Your bag</h2>
         <div>
-            {
-                cartItems.map((item) => {
-                  return <CartItem key={item.id} {...item} />
-                })
-            }
+          {cartItems.map((item) => {
+            return <CartItem key={item.id} {...item} />;
+          })}
         </div>
         <footer>
-            <hr />
-            <div className="cart-total">
-                <h4>
-                    total <span>${total}</span>
-                </h4>
-            </div>
-            <button className="btn clear-btn">clear</button>
+          <hr />
+          <div className="cart-total">
+            <h4>
+              total <span>${total}</span>
+            </h4>
+          </div>
+          <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+            clear
+          </button>
         </footer>
-    </header>
-  
-  </section>
+      </header>
+    </section>
+  );
 }
 
 export default CartContainer;
